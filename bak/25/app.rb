d8b20376d89f3fc89712a169ae4d5444cac21a2e
everinArt <everinartdik@gmail.com>
2022-06-01 21:40:28 +0300

    Add app.rb Lesson 25

--- bak/25/app.rb
+++ bak/25/app.rb
@@ -1,2 +1,11 @@
 require 'sqlite3'
 db = SQLite3::Database.new 'test.sqlite'
+
+# db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 14500)"
+
+db.execute "SELECT * FROM Cars" do |car|
+	puts car
+	puts "========"
+end 
+
+db.close
